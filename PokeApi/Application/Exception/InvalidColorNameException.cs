871f7e4454559b8529bcfdece7b485018a97e056
YarleiWillersDev <yarlei.willers@gmail.com>
2026-03-23 14:43:26 -0300

    feat: implementação de tratamento de erros global e exceções personalizadas
    - criação da classe base AppException para padronização de exceções
    - criação da exceção personalizada PokeApiException herdando de AppException
    - implementação do UniversalAppExceptionHandler para tratamento centralizado de exceções
    - criação de handler padrão para erros não mapeados
    - registro do middleware de tratamento de erros no Program.cs
    - aplicação das exceções personalizadas na camada de servisse

---
+++ PokeApi/Application/Exception/InvalidColorNameException.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokeApiTeste.Exceptions;
+
+namespace PokeApiTeste.Application.Exceptions
+{
+    public class InvalidColorNameException : AppException
+    {
+        public InvalidColorNameException(string message = "Nome da cor inválido vindo da API.")
+            : base(502, "Invalid External Data", message) { }
+
+        public InvalidColorNameException(string message, Exception inner)
+            : base(502, "Invalid External Data", message, inner) { }
+    }
+}
